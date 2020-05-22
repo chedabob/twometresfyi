21439a84d177c93d48f533ce145c431926d57a95
Matt Malone <mattm@apadmi.com>
2020-05-22 16:59:52 +0100

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,38 +1,29 @@
-const conversions = {
-    "iPhone XRs" :  "13.253",
-    "Old Routemaster buses" : "0.45662",
-    "New Routemaster buses": "0.45558",
-    "Eiffel Towers (without the tip)": "0.00625",
-    "Eiffel Towers (with the tip)": "0.006172839506",
-    "Furlongs": "0.009941939076",
-    "Big Macs" : "28.632",
-    "Bed": "1",
-    "Benches": "2",
-    "Fridges": "3",
-    "Chairs": "4",
-    "Sturgeons (the politician)": 1.22,
-    "Sturgeons (the fish)" : 10
-}
+const conversions = [
+    "13.253 iPhone XRs",
+    "0.45662 Old Routemaster buses",
+    "0.45558 New Routemaster buses",
+    "0.00625 Eiffel Towers (without the tip)",
+    "0.006172839506 Eiffel Towers (with the tip)",
+    "0.009941939076 Furlongs",
+    "28.632 Big Macs",
+    "1 Bed",
+    "2 Benches",
+    "3 Fridges",
+    "4 Chairs",
+    "1.22 Sturgeons (the politician)",
+    "10 Sturgeons (the fish)",
+    "0.00000000000000000000000022731200731712534% of the observable universe's diameter"
+]
 
-var randomProperty = function (obj) {
-    var keys = Object.keys(obj);
-    var key = keys[ keys.length * Math.random() << 0]
-    var selection = obj[key]
-    return { 
-        "text": key,
-        "value": selection
-    };
+var randomVal = function (obj) {
+    return obj[ obj.length * Math.random() << 0]
 };
 
-var format = function (selection) {
-    return `${parseFloat(selection.value)} ${selection.text}`
-}
-
 var rotate = function () {
-    let selection = randomProperty(conversions)
+    let selection = randomVal(conversions)
     let bigText = document.getElementById("BigText")
     
-    bigText.innerText = format(selection)
+    bigText.innerText = selection
 }
 
 document.addEventListener("DOMContentLoaded",function(){
